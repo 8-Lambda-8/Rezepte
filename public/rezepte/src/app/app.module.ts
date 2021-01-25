280bb908364bff53b6c70f270c9e5f1595404139
8-Lambda-8 <j.wasle111@gmail.com>
2021-01-25 23:24:29 +0100

    added header and login

--- public/rezepte/src/app/app.module.ts
+++ public/rezepte/src/app/app.module.ts
@@ -1,18 +1,91 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+/* External Modules */
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+
+/* Components */
+import { AppComponent } from './app.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ListRecipeComponent } from './list-recipe/list-recipe.component';
+import { ListCategoryComponent } from './list-category/list-category.component';
+import { IngredientArrayComponent } from './ingredient-array/ingredient-array.component';
+import { IngredientSelectorComponent } from './ingredient-selector/ingredient-selector.component';
+import { AddIngredientComponent } from './add-ingredient/add-ingredient.component';
+import { EditIngredientsComponent } from './edit-ingredients/edit-ingredients.component';
+
+
+import { environment } from 'src/environments/environment';
+
+/* Material Components  */
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { LoginComponent } from './login/login.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from './header/header.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRecipeComponent,
+    RecipeComponent,
+    ListRecipeComponent,
+    ListCategoryComponent,
+    IngredientArrayComponent,
+    IngredientSelectorComponent,
+    AddIngredientComponent,
+    EditIngredientsComponent,
+    LoginComponent,
+    HeaderComponent,
+    ProfileCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
