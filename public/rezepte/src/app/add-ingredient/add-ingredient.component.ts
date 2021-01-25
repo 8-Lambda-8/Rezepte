d5a639fcb76890c640c25ba0a33be2235dd22a74
8-Lambda-8 <j.wasle111@gmail.com>
2021-01-25 23:25:39 +0100

    added edit and add ingridient Components

---
+++ public/rezepte/src/app/add-ingredient/add-ingredient.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from "../models/ingredient";
+import { ingredientsService } from "../service/ingredients.service";
+import { UNITS } from "../possible-units";
+import { FormControl } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-add-ingredient',
+  templateUrl: './add-ingredient.component.html',
+  styleUrls: ['./add-ingredient.component.scss']
+})
+
+export class AddIngredientComponent implements OnInit {
+
+  constructor(
+    private ingredientsService: ingredientsService,
+  ) { }
+  
+  name = new FormControl();
+  units = new FormControl();
+
+  unitList: string[] = UNITS;
+  ingredient: Ingredient = {id:"", name: "", possibleUnits: []};
+
+  ngOnInit(): void {
+    /* this.getIngredients(); */
+  }
+
+  /* getIngredients() {
+    this.ingredientsService.getIngredients().subscribe(item => console.log(item));
+  } */
+
+  onSubmit() {
+    if(this.ingredient.name!=""&&this.ingredient.possibleUnits!=[]){
+      this.ingredientsService.addItem(this.ingredient)
+      this.ingredient.name = "";
+      this.ingredient.possibleUnits = [];
+    }
+  }
+
+}
