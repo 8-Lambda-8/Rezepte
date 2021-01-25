4838b2df28026b80be4ad79daddab9c924f3ca90
8-Lambda-8 <j.wasle111@gmail.com>
2021-01-25 23:24:29 +0100

    added header and login

---
+++ public/rezepte/src/app/header/header.component.ts
@@ -0,0 +1,74 @@
+import { catchError, take } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { EMPTY, Observable, of } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import firebase from 'firebase/app';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser'; 
+
+import { AuthService } from '../service/auth.service';
+//import { FEED } from './../../consts/routes.const';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  user$: Observable<firebase.User> = this.auth.user$;
+
+  constructor(
+    private readonly auth: AuthService,
+    private readonly snackBar: MatSnackBar,
+    private readonly router: Router,
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer,
+    
+  ) {
+    this.registerProviderIcons();
+  }
+
+  login() {
+    this.auth
+      .loginViaGoogle()
+      .pipe(
+        take(1),
+        catchError((error) => {
+          this.snackBar.open(`${error.message}`, 'Close', {
+            duration: 4000,
+          });
+          return EMPTY;
+        }),
+      )
+      .subscribe(
+        (response) =>
+          response &&
+          this.snackBar.open(
+            'Logged in',
+            'Close',
+            {
+              duration: 4000,
+            },
+          ),
+      );
+  }
+
+  logout() {
+    this.auth
+      .logout()
+      .pipe(take(1))
+      .subscribe((response) => {
+        //this.router.navigate([`/${FEED}`]);
+        this.snackBar.open('Loged out', 'Close', {
+          duration: 4000,
+        });
+      });
+  }
+
+  registerProviderIcons() {
+    this.iconRegistry
+      .addSvgIcon('google-colored', this.sanitizer.bypassSecurityTrustResourceUrl('../assets/google.svg'))
+  }
+
+}
