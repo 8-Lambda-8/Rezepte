e58530546352efec2b1b36ac42c761b732c8dc82
8-Lambda-8 <j.wasle111@gmail.com>
2021-02-01 17:08:46 +0100

    added edit button in recipe

--- public/rezepte/src/app/recipe/recipe.component.ts
+++ public/rezepte/src/app/recipe/recipe.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Recipe } from '../models/recipe';
 import { RecipeService } from '../service/recipe.service';
+import { UserData } from '../models/userData';
+import { AuthService } from '../service/auth.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-recipe',
@@ -15,10 +18,19 @@ export class RecipeComponent implements OnInit {
 
   displayedColumns: string[] = ['name', 'amount', 'unit'];
   
+  myUserData: UserData =  {uid:"",name:"",permissionClass:0,photoURL:"",email:""};
+  
   constructor(
     private route: ActivatedRoute,
     private recipeService: RecipeService,
-  ) { }
+    private db: AngularFirestore,
+    private readonly auth: AuthService,
+    
+  ) { 
+    this.auth.user$.subscribe(user =>{
+      this.db.collection('users').doc<UserData>(user.uid).valueChanges().subscribe(data=>this.myUserData=data)
+    });
+  }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
